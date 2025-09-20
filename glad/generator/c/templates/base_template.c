da6912843ccb72914f0a27be5df5cd0eccc905b5
k0michi <k0michi@koyomi.co>
2025-09-20 16:25:01 +0900

    Make context fields thread local
    See https://github.com/Dav1dde/glad/issues/292

--- glad/generator/c/templates/base_template.c
+++ glad/generator/c/templates/base_template.c
@@ -24,7 +24,7 @@ extern "C" {
 {% block variables %}
 {% if options.mx_global %}
 {% call template_utils.zero_initialized() %}Glad{{ feature_set.name|api }}Context {{ global_context }}_static{% endcall %}
-Glad{{ feature_set.name|api }}Context* {{ global_context }} = &{{ global_context }}_static;
+_Thread_local Glad{{ feature_set.name|api }}Context* {{ global_context }} = &{{ global_context }}_static;
 {% endif %}
 {% endblock %}
 
--- glad/generator/c/templates/base_template.h
+++ glad/generator/c/templates/base_template.h
@@ -85,7 +85,7 @@ typedef struct Glad{{ feature_set.name|api }}Context {
 } Glad{{ feature_set.name|api }}Context;
 
 {% if options.mx_global %}
-GLAD_API_CALL Glad{{ feature_set.name|api }}Context* glad_{{ feature_set.name }}_context;
+GLAD_API_CALL _Thread_local Glad{{ feature_set.name|api }}Context* glad_{{ feature_set.name }}_context;
 
 {% for extension in chain(feature_set.features, feature_set.extensions) %}
 #define GLAD_{{ extension.name }} (glad_{{ feature_set.name }}_context->{{ extension.name|no_prefix }})
